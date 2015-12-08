6715a2b0ef3c206bfdf4a3953b608bb53ccc1094
joranvar <bart.post@gmail.com>
2015-12-08 22:10:09 +0100

    Remove unchanging parameters from recursion

--- asciitree.fsx
+++ asciitree.fsx
@@ -5,23 +5,25 @@
     //if trunk and counter = 0 --> split and transition to branch
     //if branch and counter > 0 --> write more branch
     //if branch and counter = 0 --> transition to trunk and deecrement max
-    let rec tree l w h max original counter branch roots acc =
+    let tree l w h max original counter branch roots acc =
         let advanceBranch x = Array.mapi (fun i elem -> if isEven i then elem-1 else elem+1) x
         let splitRoots x = Array.collect (fun elem -> [|elem-1;elem+1|]) x
         let treeLine oneLocs w =
             String.concat "" (Seq.map (fun x -> if Array.exists (fun elem -> elem = x) oneLocs then "1" else "_") w)
 
-        match branch with
-        | _ when max = 0 ->
-            acc
-        | false when counter > 0 ->
-            tree l w h max original (counter-1) false roots (acc @ [ treeLine roots (seq { 1 .. w }) ])
-        | false when counter = 0 ->
-            tree l w h max original (original-1) true (splitRoots roots) acc
-        | true when counter > 0 ->
-            tree l w h max original (counter-1) true (advanceBranch roots) (acc @ [ treeLine roots (seq { 1 .. w })])
-        | true when counter = 0 ->
-            tree l w h (max-1) (original/2) (original/2) false roots (acc @ [ treeLine roots (seq { 1 .. w }) ])
+        let rec tree' max original counter branch roots acc =
+            match branch with
+            | _ when max = 0 ->
+                acc
+            | false when counter > 0 ->
+                tree' max original (counter-1) false roots (acc @ [ treeLine roots (seq { 1 .. w }) ])
+            | false when counter = 0 ->
+                tree' max original (original-1) true (splitRoots roots) acc
+            | true when counter > 0 ->
+                tree' max original (counter-1) true (advanceBranch roots) (acc @ [ treeLine roots (seq { 1 .. w })])
+            | true when counter = 0 ->
+                tree' (max-1) (original/2) (original/2) false roots (acc @ [ treeLine roots (seq { 1 .. w }) ])
+        tree' max original counter branch roots acc
 
     //63 rows
     //100 cols
